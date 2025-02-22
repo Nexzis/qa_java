cef888d7621f24d31f04b74857eb12be659ab3b6
Roman <singarti@yandex.com>
2025-02-22 23:02:44 +0300

    add last file  of tests

---
+++ src/test/java/FelineTest.java
@@ -0,0 +1,34 @@
+import com.example.Feline;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class FelineTest {
+
+    @Test
+    public void testFelineEatMeat() throws Exception {
+        Feline feline = new Feline();
+        List<String> expectedFood = List.of("Животные", "Птицы", "Рыба");
+        assertEquals(expectedFood, feline.eatMeat());
+    }
+
+    @Test
+    public void testFelineGetFamily() {
+        Feline feline = new Feline();
+        assertEquals("Кошачьи", feline.getFamily());
+    }
+
+    @Test
+    public void testFelineGetKittensDefault() {
+        Feline feline = new Feline();
+        assertEquals(1, feline.getKittens());
+    }
+
+    @Test
+    public void testFelineGetKittensWithParam() {
+        Feline feline = new Feline();
+        assertEquals(3, feline.getKittens(3));
+    }
+}
